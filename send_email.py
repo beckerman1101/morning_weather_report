bc836a2352e03fe299780b0b443eee273ec62015
beckerman1101 <143034659+beckerman1101@users.noreply.github.com>
2025-02-24 14:13:42 -0700

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -45,7 +45,7 @@ def send_email_with_attachment(sender_email, receiver_emails, subject, body, att
 
 # Example usage
 sender_email = f"{GOOGLE_EMAIL}"
-receiver_emails = EMAIL_RECIPIENTS  # Add multiple emails in a list
+receiver_emails = f"{EMAIL_RECIPIENTS}"  # Add multiple emails in a list
 subject = "24-Hour Snowfall Report"
 body = "This email is automated. Attached is the report for snowfall statewide in the past 24 hours. Data is preliminary and has not been refined for quality control."
 attachment_file_path = path  # Make sure the path is correct
