070d317c10a748ec4297396237893f0db87eefd5
beckerman1101 <143034659+beckerman1101@users.noreply.github.com>
2025-02-24 13:22:34 -0700

    Update daily_accum_map.py

--- daily_accum_map.py
+++ daily_accum_map.py
@@ -27,9 +27,6 @@ os.environ['SHAPE_RESTORE_SHX'] = 'YES'
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
 # In[2]:
-GOOGLE_EMAIL = os.getenv("GOOGLE_EMAIL")
-GOOGLE_PASSWORD = os.getenv("GOOGLE_PASSWORD")
-EMAIL_RECIPIENTS = os.getenv("EMAIL_RECIPIENTS")
 
 #Map Variable Setup
 
@@ -211,60 +208,3 @@ fig.text(0.028, 0.92, f'Valid: 5am {yesterdayst} - 5am {todayst}', fontsize=85)
 fig.figimage(cdotlogo, 5800, 6080, zorder=35)
 path = os.path.join(base_dir, 'daily_file', f'24houraccum_{todaystr}.png')
 plt.savefig(path, bbox_inches='tight', pad_inches=0.01)
-
-
-# In[5]:
-
-
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-
-def send_email_with_attachment(sender_email, receiver_emails, subject, body, attachment_file_path):
-    # Create the email message
-    msg = MIMEMultipart()
-    msg['From'] = sender_email
-    msg['To'] = ", ".join(receiver_emails)  # Convert list to comma-separated string
-    msg['Subject'] = subject
-
-    # Attach the body of the email
-    msg.attach(MIMEText(body, 'plain'))
-
-    # Open the attachment file in binary mode
-    with open(attachment_file_path, 'rb') as attachment_file:
-        # Attach the file to the email
-        part = MIMEBase('application', 'octet-stream')
-        part.set_payload(attachment_file.read())
-        encoders.encode_base64(part)  # Encode the attachment
-        part.add_header('Content-Disposition', f'attachment; filename={attachment_file_path.split("/")[-1]}')
-        msg.attach(part)
-
-    # Send the email
-    try:
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-            server.login(sender_email, f"{GOOLE_PASSWORD}")  # Use an app password if 2FA is enabled
-            server.sendmail(sender_email, receiver_emails, msg.as_string())
-            print(f'Email sent successfully to: {", ".join(receiver_emails)} with attachment: {attachment_file_path}')
-    except Exception as e:
-        print(f"Failed to send email: {e}")
-
-# Example usage
-sender_email = f"{GOOGLE_EMAIL}"
-receiver_emails = EMAIL_RECIPIENTS  # Add multiple emails in a list
-subject = "24-Hour Snowfall Report"
-body = "This email is automated. Attached is the report for snowfall statewide in the past 24 hours. Data is preliminary and has not been refined for quality control."
-attachment_file_path = path  # Make sure the path is correct
-
-send_email_with_attachment(sender_email, receiver_emails, subject, body, attachment_file_path)
-
-
-
-
-
-# In[ ]:
-
-
-
-
