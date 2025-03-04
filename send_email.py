ceb532507e130be1d3224c8b2cc3d24190fd9744
beckerman1101 <143034659+beckerman1101@users.noreply.github.com>
2025-03-03 17:31:08 -0700

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -5,7 +5,8 @@ from git import Repo
 
 # GitHub configuration
 GITHUB_TOKEN = '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'
-REPO_NAME = os.path.dirname(os.path.abspath(__file__))  # Replace with your repository
+g = Github(GITHUB_TOKEN)
+REPO_NAME = g.get_repo('beckerman1101/daily_accum_mapping')  # Replace with your repository
 BRANCH_NAME = 'main'  # Adjust if needed
 FILE_PATH_IN_REPO = 'daily_file/filename.tar.gz'  # Path where the file will be saved in your repo
 DOWNLOAD_URL = 'https://tgftp.nws.noaa.gov/SL.us008001/DF.sha/DC.cap/DS.WWA/current_all.tar.gz'
